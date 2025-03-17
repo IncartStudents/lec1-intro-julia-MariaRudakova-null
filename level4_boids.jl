4c9effe27897e560ba57858c40f1973880585ff0
Maria Rudakova <roudakova.maria@gmail.com>
2025-03-17 10:25:25 +0300

    There is a problem

--- level4_boids.jl
+++ level4_boids.jl
@@ -1,24 +1,56 @@
 module Boids
+
 using Plots
 
+mutable struct Boid
+    positionX::Float64  # Координата X
+    positionY::Float64  # Координата Y
+    velocityX::Float64  # Скорость по оси X
+    velocityY::Float64  # Скорость по оси Y
+end
 
 mutable struct WorldState
-    boid::Tuple{Float64, Float64}
+    boids::Vector{Boid}
     height::Float64
     width::Float64
+
     function WorldState(n_boids, width, height)
-        # TODO: добавить случайные позиции для n_boids птичек вместо одной
-        
-        new((width/2, height/2), width, height)
+        boids = Vector{Boid}(undef, n_boids)
+        for i in 1:n_boids
+            pos_x = rand() * width
+            pos_y = rand() * height
+            vel_x = (rand() * 2 - 1) * 5
+            vel_y = (rand() * 2 - 1) * 5
+            boids[i] = Boid(pos_x, pos_y, vel_x, vel_y)
+        end
+        new(boids, width, height)
     end
 end
 
 function update!(state::WorldState)
-    state.boid = state.boid .+ 0.1
-    # TODO: реализация уравнения движения птичек
+    # Собираем координаты всех бодисов
+    positions = [(boid.positionX, boid.positionY) for boid in state.boids]
 
-    return nothing
+    for i in 1:length(state.boids)
+        boid = state.boids[i]
+
+        # Обновляем позицию
+        boid.positionX += boid.velocityX
+        boid.positionY += boid.velocityY
+
+        # Проверяем границы и отражаем движение
+        if boid.positionX < 0 || boid.positionX > state.width
+            boid.velocityX = -boid.velocityX
+        end
+        if boid.positionY < 0 || boid.positionY > state.height
+            boid.velocityY = -boid.velocityY
+        end
+    end
+
+    return positions  # Возвращаем список координат бодисов
 end
+
+# Без изменений ниже этой строки
 
 function (@main)(ARGS)
     w = 30
@@ -28,8 +60,7 @@ function (@main)(ARGS)
     state = WorldState(n_boids, w, h)
 
     anim = @animate for time = 1:100
-        update!(state)
-        boids = [state.boid]
+        boids = update!(state)
         scatter(boids, xlim = (0, state.width), ylim = (0, state.height))
     end
     gif(anim, "boids.gif", fps = 10)
